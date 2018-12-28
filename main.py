5b37327f0f31c820bc2637f127e5e84f73f96acb
gajeelsf <anepycn@gmail.com>
2018-12-28 00:45:03 -0800

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 env_id = "Swimmer-v2"
-seed = 0
-reward_scale = 1
+seed = 1 
+reward_scale=0.5 
 os.system('python -m baselines.fcn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
 os.system('python -m baselines.scn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
 os.system('python -m baselines.ppo1.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
