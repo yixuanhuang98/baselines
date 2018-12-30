64284bc220b78b4d6a88260ba33830ea9ed4331b
gajeelSF <anepycn@gmail.com>
2018-12-30 11:31:17 -0800

    .

--- main_hc2.py
+++ main_hc2.py
@@ -1,7 +1,7 @@
 import os
 env_id = "Swimmer-v2"
-seed_list = [2,3,4,5]
-reward_scale_list=[0.1,0.5,1]
+seed_list = [4,5]
+reward_scale_list=[0.1,1]
 for seed in seed_list:
     for reward_scale in reward_scale_list:
         os.system('python -m baselines.fcn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
