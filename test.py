83a7ec94fbceebd9ecc3385dc15a6f94ff7014af
gajeelSF <anepycn@gmail.com>
2019-01-03 14:05:15 -0800

    .

--- test.py
+++ test.py
@@ -4,5 +4,4 @@ seed_list = [1,2,3]
 reward_scale_list=[0.1]
 for seed in seed_list:
     for reward_scale in reward_scale_list:
-        os.system('python -m baselines.fcn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
-        os.system('python -m baselines.linear.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+        os.system('python -m baselines.fcn2.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
