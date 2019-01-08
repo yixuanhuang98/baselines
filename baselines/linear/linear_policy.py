efdfaa3d0f0b8acd8fff8dad701474255aeceb63
gajeelSF <anepycn@gmail.com>
2019-01-07 23:04:28 -0800

    .

--- baselines/linear/linear_policy.py
+++ baselines/linear/linear_policy.py
@@ -11,7 +11,7 @@ class LinearPolicy(object):
             self._init(*args, **kwargs)
             self.scope = tf.get_variable_scope().name
 
-    def _init(self, ob_space, ac_space, hid_size, num_hid_layers, gaussian_fixed_var=True):
+    def _init(self, ob_space, ac_space, hid_size, num_hid_layers, gaussian_fixed_var=True, num_actors=1):
         assert isinstance(ob_space, gym.spaces.Box)
 
         self.pdtype = pdtype = make_pdtype(ac_space)
