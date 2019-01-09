b4455291b4f0d9effc240942a618687ca904a73a
gajeelSF <anepycn@gmail.com>
2019-01-08 19:57:41 -0800

    add fcn to compare

--- baselines/fcn/fcn_policy.py
+++ baselines/fcn/fcn_policy.py
@@ -4,18 +4,16 @@ import tensorflow as tf
 import gym
 from baselines.common.distributions import make_pdtype, CategoricalPdType
 
-
-class MlpPolicy(object):
+class FcnPolicy(object):
     recurrent = False
     def __init__(self, name, *args, **kwargs):
+        self.name = name
         with tf.variable_scope(name):
             self._init(*args, **kwargs)
             self.scope = tf.get_variable_scope().name
 
-    def _init(self, ob_space, ac_space, hid_size, num_hid_layers, gaussian_fixed_var=True):
+    def _init(self, ob_space, ac_space, num_actors, hid_size, num_hid_layers, gaussian_fixed_var=True):
         assert isinstance(ob_space, gym.spaces.Box)
-
-        num_actors = 1
 
         self.cpdtype = cpdtype = CategoricalPdType(num_actors)
         self.pdtype = pdtype = make_pdtype(ac_space)
@@ -33,7 +31,7 @@ class MlpPolicy(object):
                 last_out = tf.nn.tanh(tf.layers.dense(last_out, hid_size, name="fc%i"%(i+1), kernel_initializer=U.normc_initializer(1.0)))
             self.vpred = tf.layers.dense(last_out, 1, name='final', kernel_initializer=U.normc_initializer(1.0))[:,0]
 
-        '''with tf.variable_scope('dec'):
+        with tf.variable_scope('dec'):
             last_out = obz
             last_out = tf.layers.dense(last_out, hid_size, name='dec', kernel_initializer=U.normc_initializer(1.0))
 
@@ -45,17 +43,33 @@ class MlpPolicy(object):
         self.cpd = cpdtype.pdfromflat(hidden_decision)
         #TODO: not sure of sampling or mode
         self.choice = ch =self.cpd.sample()
-        '''
+
         with tf.variable_scope('pol'):
-            last_out = obz
-            for i in range(num_hid_layers):
-                last_out = tf.layers.dense(last_out, hid_size, name='fc%i'%(i+1), kernel_initializer=U.normc_initializer(1.0))
+            last_outs = []
+            actors = []
+            for i in range(num_actors):
+                last_outs.append(tf.layers.dense(obz, hid_size, name='sub%i'%(i+1), kernel_initializer=U.normc_initializer(1.0)))
+
+            ch = tf.reshape(ch,[-1])
+            r = tf.range(tf.shape(ch)[0])
+            ch = tf.cast(ch,tf.int32)
+
+            ch_nd = tf.stack([ch,r],axis=1)
+
             if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
-                mean = tf.layers.dense(last_out, pdtype.param_shape()[0]//2, name='final', kernel_initializer=U.normc_initializer(0.01))
+                for i in range(num_actors):
+                    actors.append(tf.layers.dense(last_outs[i],pdtype.param_shape()[0]//2,name="final%i"%(i+1),kernel_initializer=U.normc_initializer(0.01)))
+                self.actors = tf.stack(actors)
+
+                mean = tf.gather_nd(self.actors, ch_nd)
                 logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
                 pdparam = tf.concat([mean, mean * 0.0 + logstd], axis=1)
+
             else:
-                pdparam = tf.layers.dense(last_out, pdtype.param_shape()[0], name='final', kernel_initializer=U.normc_initializer(0.01))
+                for i in range(num_actors):
+                    actors.append(tf.layers.dense(last_outs[i],pdtype.param_shape()[0],name="final%i"%(i+1),kernel_initializer=U.normc_initializer(0.01)))
+                self.actors = tf.stack(actors)
+                pdparam = tf.gather_nd(self.actors, ch_nd)
 
         self.pd = pdtype.pdfromflat(pdparam)
 
@@ -64,11 +78,30 @@ class MlpPolicy(object):
 
         stochastic = tf.placeholder(dtype=tf.bool, shape=())
         ac = U.switch(stochastic, self.pd.sample(), self.pd.mode())
-        self._act = U.function([stochastic, ob], [ac, self.vpred])
+
+        self._act = U.function([stochastic, ob], [ac,ch,self.vpred])
 
     def act(self, stochastic, ob):
-        ac1, vpred1 =  self._act(stochastic, ob[None])
-        return ac1[0], vpred1[0]
+        ac1,ch1, vpred1 =  self._act(stochastic, ob[None])
+        return ac1[0],ch1, vpred1[0]
+
+    def pd_given_ch(self, choice,ac_space, gaussian_fixed_var=True):
+        choice = tf.reshape(choice,[-1])
+        choice = tf.cast(choice,tf.int32)
+
+        r = tf.range(tf.shape(choice)[0])
+        ch_nd = tf.stack([choice,r],axis=1)
+        if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
+            mean = tf.gather_nd(self.actors, ch_nd)
+
+            with tf.variable_scope(self.name, reuse=True):
+                with tf.variable_scope('pol', reuse=True):
+                    logstd = tf.get_variable(name="logstd", shape=[1, self.pdtype.param_shape()[0]//2], initializer=tf.zeros_initializer())
+                    pdparams = tf.concat([mean, mean * 0.0 + logstd], axis=1)
+        else:
+            pdparams = tf.gather_nd(self.actors, ch_nd)
+        return self.pdtype.pdfromflat(pdparams)
+
     def get_variables(self):
         return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.scope)
     def get_trainable_variables(self):
