7dac681d32a65b958a2fd5a8e511e0babb3c66ab
gajeelSF <anepycn@gmail.com>
2019-01-08 20:51:22 -0800

    add lc

--- plot.py
+++ plot.py
@@ -8,14 +8,14 @@ for seed in seeds:
     for reward_scale in reward_scales:
         linear_rewards = np.loadtxt('./baselines/ppo1/data/'+env_id+'_s'+str(seed)+'_r'+str(reward_scale)+'_rew.txt')* (1 / reward_scale)
         linear_ts = np.loadtxt('./baselines/ppo1/data/'+env_id+'_s'+str(seed)+'_r'+str(reward_scale)+'_ts.txt')
-        lc_rewards = np.loadtxt('./baselines/ppo1/data/'+env_id+'_s'+str(seed)+'_r'+str(reward_scale)+'_rew_lc.txt')* (1 / reward_scale)
-        lc_ts = np.loadtxt('./baselines/ppo1/data/'+env_id+'_s'+str(seed)+'_r'+str(reward_scale)+'_ts_lc.txt')
+        fcn_rewards = np.loadtxt('./baselines/fcn/data/'+env_id+'_s'+str(seed)+'_r'+str(reward_scale)+'_rew.txt')* (1 / reward_scale)
+        fcn_ts = np.loadtxt('./baselines/fcn/data/'+env_id+'_s'+str(seed)+'_r'+str(reward_scale)+'_ts.txt')
 
 
         fig, ax = plt.subplots()
 
         ax.plot(linear_ts, linear_rewards,  label='LINEAR')
-        ax.plot(lc_ts, lc_rewards,  label='LC')
+        ax.plot(fcn_ts, fcn_rewards,  label='fcn-1')
 
         legend = ax.legend(loc='lower right')
 
