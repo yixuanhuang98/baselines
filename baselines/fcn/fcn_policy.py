67f264fa28850c7aa098cfed40b75bdc841c0e3e
gajeelSF <anepycn@gmail.com>
2019-01-10 13:39:12 -0800

    fix

--- baselines/fcn/fcn_policy.py
+++ baselines/fcn/fcn_policy.py
@@ -54,7 +54,7 @@ class FcnPolicy(object):
 
             ch = tf.reshape(ch,[-1])
             r = tf.range(tf.shape(ch)[0])
-            ch = tf.zeros(shape=tf.shape(ch))
+            ch = tf.cast(ch,tf.int32)
             ch_nd = tf.stack([ch,r],axis=1)
 
             if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
