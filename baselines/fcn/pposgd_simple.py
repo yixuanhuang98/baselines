65f6c887d384c96e8aff3327a87f1c1d22a585ca
gajeelsf <anepycn@gmail.com>
2019-01-11 11:14:40 -0800

    Changes for s basic with 2 different loops for actors and decision.

--- baselines/fcn/pposgd_simple.py
+++ baselines/fcn/pposgd_simple.py
@@ -123,26 +123,26 @@ def learn(env, policy_fn, *,
     ac_ratio = tf.exp(pi.pd.logp(ac) - old_pi_pd.logp(ac)) # pnew / pold
     ch_ratio = tf.exp(pi.cpd.logp(ch) - oldpi.cpd.logp(ch))
 
-    surr1 = ac_ratio * ch_ratio * atarg # surrogate from conservative policy iteration
-    surr2 = tf.clip_by_value(ac_ratio, 1.0 - clip_param, 1.0 + clip_param) * ch_ratio * atarg #
-    surr3 = tf.clip_by_value(ch_ratio, 1.0 - clip_param, 1.0 + clip_param) * ac_ratio * atarg
-    surr4 = tf.clip_by_value(ch_ratio, 1.0 - clip_param, 1.0 + clip_param) * tf.clip_by_value(ac_ratio, 1.0 - clip_param, 1.0 + clip_param) * atarg
+    surr11 = ch_ratio * atarg # surrogate from conservative policy iteration
+    #surr2 = tf.clip_by_value(ac_ratio, 1.0 - clip_param, 1.0 + clip_param) * ch_ratio * atarg #
+    surr12 = tf.clip_by_value(ch_ratio, 1.0 - clip_param, 1.0 + clip_param) * atarg
+    #surr14 = tf.clip_by_value(ch_ratio, 1.0 - clip_param, 1.0 + clip_param) * tf.clip_by_value(ac_ratio, 1.0 - clip_param, 1.0 + clip_param) * atarg
 
 
     surr21 = ac_ratio * atarg # surrogate from conservative policy iteration
     surr22 = tf.clip_by_value(ac_ratio, 1.0 - clip_param, 1.0 + clip_param) * atarg #
-    pol_surr = - tf.reduce_mean(tf.minimum(tf.minimum(tf.minimum(surr1, surr2),surr3),surr4)) # PPO's pessimistic surrogate (L^CLIP)
+    pol_surr1 = - tf.reduce_mean(tf.minimum(surr11, surr12)) # PPO's pessimistic surrogate (L^CLIP)
     pol_surr2 = - tf.reduce_mean(tf.minimum(surr21, surr22)) # PPO's pessimistic surrogate (L^CLIP)
     vf_loss = tf.reduce_mean(tf.square(pi.vpred - ret))
-    total_loss = pol_surr + pol_entpen + vf_loss
+    total_loss1 = pol_surr1 + pol_entpen + vf_loss
     total_loss2 = pol_surr2 + pol_entpen + vf_loss
-    losses = [pol_surr, pol_entpen, vf_loss, meankl, meanent]
+    losses = [pol_surr1, pol_entpen, vf_loss, meankl, meanent]
     loss_names = ["pol_surr", "pol_entpen", "vf_loss", "kl", "ent"]
 
     var_list = pi.get_trainable_variables()
     #var_list2 = [var for var in var_list if not('dec' in var.name)]
 
-    lossandgrad = U.function([ob, ac,ch, atarg, ret, lrmult], losses + [U.flatgrad(total_loss, var_list)])
+    lossandgrad = U.function([ob, ac,ch, atarg, ret, lrmult], losses + [U.flatgrad(total_loss1, var_list)])
     lossandgrad2 = U.function([ob, ac,ch, atarg, ret, lrmult], losses + [U.flatgrad(total_loss2, var_list)])
     adam = MpiAdam(var_list, epsilon=adam_epsilon)
 
@@ -170,8 +170,6 @@ def learn(env, policy_fn, *,
     timestep_list = []
 
     while True:
-        print(env_name)
-
         if callback: callback(locals(), globals())
         if max_timesteps and timesteps_so_far >= max_timesteps:
             break
@@ -190,12 +188,10 @@ def learn(env, policy_fn, *,
             raise NotImplementedError
 
 
-        is_learn_choice = ((iters_so_far % 10) == 0) and (iters_so_far < 800)
-        print(is_learn_choice)
+        is_learn_choice = ((iters_so_far % 2) == 0)
         logger.log("********** Iteration %i ************"%iters_so_far)
 
         seg = seg_gen.__next__()
-        seg = seg_gen.send(is_learn_choice)
         add_vtarg_and_adv(seg, gamma, lam)
 
         # ob, ac, atarg, ret, td1ret = map(np.concatenate, (obs, acs, atargs, rets, td1rets))
@@ -254,8 +250,8 @@ def learn(env, policy_fn, *,
         if MPI.COMM_WORLD.Get_rank()==0:
             logger.dump_tabular()
 
-    np.savetxt('./baselines/fcn/data/'+env_name+'_s'+str(seed)+'_r'+str(scale)+'_rew_sbasic.txt',np.asarray(reward_list))
-    np.savetxt('./baselines/fcn/data/'+env_name+'_s'+str(seed)+'_r'+str(scale)+'_ts_sbasic.txt',np.asarray(timestep_list))
+    np.savetxt('./baselines/fcn/data/'+env_name+'_s'+str(seed)+'_r'+str(scale)+'_rew_scbasic.txt',np.asarray(reward_list))
+    np.savetxt('./baselines/fcn/data/'+env_name+'_s'+str(seed)+'_r'+str(scale)+'_ts_scbasic.txt',np.asarray(timestep_list))
 
     return pi
 
