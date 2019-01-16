9f1b7b1b5794e9006dd01fcf69ca16ace022b2de
gajeelsf <anepycn@gmail.com>
2019-01-16 12:38:19 -0800

    Adding tests for swimmer best

--- main.sh
+++ main.sh
@@ -3,3 +3,8 @@ xterm -e 'python -m baselines.fcn.run_humanoid --seed=7 --reward_scale=1.0 --env
 xterm -e 'python -m baselines.fcn.run_humanoid --seed=8 --reward_scale=1.0 --env=Walker2d-v2' &
 xterm -e 'python -m baselines.fcn.run_humanoid --seed=9 --reward_scale=1.0 --env=Walker2d-v2' &
 xterm -e 'python -m baselines.fcn.run_humanoid --seed=10 --reward_scale=1.0 --env=Walker2d-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=6 --reward_scale=1.0 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=7 --reward_scale=1.0 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=8 --reward_scale=1.0 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=9 --reward_scale=1.0 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=10 --reward_scale=1.0 --env=Swimmer-v2' &
