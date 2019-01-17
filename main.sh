cb94fcd133c005d01c5ef3e08db46b99def0c1e3
Gaurav Mahajan <gmahajan@eng.ucsd.edu>
2019-01-17 14:34:05 -0800

    r 11-15

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=6 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=7 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=8 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=9 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=10 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=11 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=12 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=13 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=14 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=15 --reward_scale=0.1 --env=HalfCheetah-v2' &
 wait
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=6 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=7 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=8 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=9 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=10 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2' &
