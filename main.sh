3a9b8b7cb08f30e1cb1cf141d49d9adaa1f85f68
gajeelsf <anepycn@gmail.com>
2019-01-16 19:56:40 -0800

    Humanoid with more seeds

--- main.sh
+++ main.sh
@@ -1,29 +1,5 @@
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=1 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=0.1 --env=HalfCheetah-v2' &
-wait
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=1 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=0.1 --env=Hopper-v2' &
-wait
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=1 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=0.1 --env=Walker2d-v2' &
-wait
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=1 --reward_scale=0.1 --env=Swimmer-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=0.1 --env=Swimmer-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=0.1 --env=Swimmer-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=0.1 --env=Swimmer-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=0.1 --env=Swimmer-v2' &
-wait
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=1 --reward_scale=0.1 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=0.1 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=0.1 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=0.1 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=0.1 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=6 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=7 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=8 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=9 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=10 --reward_scale=1.0 --env=Humanoid-v2' &
