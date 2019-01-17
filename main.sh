329e755e18c10b0e2ffdd940f5e8f0983cbe6bb3
Gaurav Mahajan <gmahajan@eng.ucsd.edu>
2019-01-17 14:35:24 -0800

    r 11-15

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=6 --reward_scale=1.0 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=7 --reward_scale=1.0 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=8 --reward_scale=1.0 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=9 --reward_scale=1.0 --env=Humanoid-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=10 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=11 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=12 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=13 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=14 --reward_scale=1.0 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=15 --reward_scale=1.0 --env=Humanoid-v2' &
