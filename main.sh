ab3d605fd0545b995d3cca7b18edfe3704f874dc
Gaurav Mahajan <gmahajan@eng.ucsd.edu>
2019-01-17 12:02:12 -0800

    Revert to xterm

--- main.sh
+++ main.sh
@@ -1,30 +1,30 @@
-python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=HalfCheetah-v2 &
-python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=HalfCheetah-v2 &
-python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=HalfCheetah-v2 &
-python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=HalfCheetah-v2 &
-python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=HalfCheetah-v2 &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=HalfCheetah-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=HalfCheetah-v2' &
 wait
-python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2 &
-python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2 &
-python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2 &
-python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2 &
-python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2 &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2' &
 wait
-python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Walker2d-v2 &
-python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Walker2d-v2 &
-python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Walker2d-v2 &
-python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Walker2d-v2 &
-python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Walker2d-v2 &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Walker2d-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Walker2d-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Walker2d-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Walker2d-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Walker2d-v2' &
 wait
-python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Swimmer-v2 &
-python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Swimmer-v2 &
-python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Swimmer-v2 &
-python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Swimmer-v2 &
-python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Swimmer-v2 &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Swimmer-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Swimmer-v2' &
 wait
-python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Humanoid-v2 &
-python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Humanoid-v2 &
-python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Humanoid-v2 &
-python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Humanoid-v2 &
-python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Humanoid-v2 &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Humanoid-v2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Humanoid-v2' &
 wait
