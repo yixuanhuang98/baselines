bf4725bd7e6222124f957681e8693e2d68d47b76
gajeelsf <anepycn@gmail.com>
2019-01-18 12:17:28 -0800

    Saving script for generalization 12-15

--- main.sh
+++ main.sh
@@ -4,11 +4,11 @@
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=19 --reward_scale=0.1 --env=HalfCheetah-v2' &
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=20 --reward_scale=0.1 --env=HalfCheetah-v2' &
 #wait
-xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_11' &
-#xterm -e 'python -m baselines.scn.run_humanoid --seed=17 --reward_scale=0.1 --env=Hopper-v2' &
-#xterm -e 'python -m baselines.scn.run_humanoid --seed=18 --reward_scale=0.1 --env=Hopper-v2' &
-#xterm -e 'python -m baselines.scn.run_humanoid --seed=19 --reward_scale=0.1 --env=Hopper-v2' &
-#xterm -e 'python -m baselines.scn.run_humanoid --seed=20 --reward_scale=0.1 --env=Hopper-v2' &
+#xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_11' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_12' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_13' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_14' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_15' &
 wait
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=16 --reward_scale=0.1 --env=Walker2d-v2' &
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=17 --reward_scale=0.1 --env=Walker2d-v2' &
