6a1b9cb8970086579210fb19566815b06c9934e8
Gaurav Mahajan <gmahajan@eng.ucsd.edu>
2019-01-17 18:09:29 -0800

    Saving model hopper

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=11 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=12 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=13 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=14 --reward_scale=0.1 --env=HalfCheetah-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=15 --reward_scale=0.1 --env=HalfCheetah-v2' &
-wait
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2' &
+# xterm -e 'python -m baselines.fcn.run_humanoid --seed=11 --reward_scale=0.1 --env=HalfCheetah-v2' &
+# xterm -e 'python -m baselines.fcn.run_humanoid --seed=12 --reward_scale=0.1 --env=HalfCheetah-v2' &
+# xterm -e 'python -m baselines.fcn.run_humanoid --seed=13 --reward_scale=0.1 --env=HalfCheetah-v2' &
+# xterm -e 'python -m baselines.fcn.run_humanoid --seed=14 --reward_scale=0.1 --env=HalfCheetah-v2' &
+# xterm -e 'python -m baselines.fcn.run_humanoid --seed=15 --reward_scale=0.1 --env=HalfCheetah-v2' &
+# wait
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/fcn_ho_11' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/fcn_ho_12' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/fcn_ho_13' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/fcn_ho_14' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/fcn_ho_15' &
