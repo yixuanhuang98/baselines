1c7dbbc17c087c04ce41d18cda97de587327c248
gajeelsf <anepycn@gmail.com>
2019-01-20 13:44:42 -0800

    save scn data

--- main.sh
+++ main.sh
@@ -3,7 +3,6 @@ xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=1.0 --env
 xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=1.0 --env=Humanoid-v2 --model-path=model/fcn_hu_3 --num_timesteps=10 --play' &
 xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=1.0 --env=Humanoid-v2 --model-path=model/fcn_hu_4 --num_timesteps=10 --play' &
 xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=1.0 --env=Humanoid-v2 --model-path=model/fcn_hu_5 --num_timesteps=10 --play' &
-wait
 xterm -e 'python -m baselines.scn.run_humanoid --seed=1 --reward_scale=0.1 --env=Humanoid-v2 --model-path=model/scn_hu_1 --num_timesteps=10 --play' &
 xterm -e 'python -m baselines.scn.run_humanoid --seed=2 --reward_scale=0.1 --env=Humanoid-v2 --model-path=model/scn_hu_2 --num_timesteps=10 --play' &
 xterm -e 'python -m baselines.scn.run_humanoid --seed=3 --reward_scale=0.1 --env=Humanoid-v2 --model-path=model/scn_hu_3 --num_timesteps=10 --play' &
