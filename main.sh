8fda8223539416bb347cac8dd5ab14227a67d9bb
Gaurav Mahajan <gmahajan@eng.ucsd.edu>
2019-01-19 20:11:42 -0800

    Script to save models 1 to 10

--- main.sh
+++ main.sh
@@ -4,11 +4,23 @@
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=19 --reward_scale=0.1 --env=HalfCheetah-v2' &
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=20 --reward_scale=0.1 --env=HalfCheetah-v2' &
 #wait
-xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_11 --num_timesteps=10 --play' &
-xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_12 --num_timesteps=10 --play' &
-xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_13 --num_timesteps=10 --play' &
-xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_14 --num_timesteps=10 --play' &
-xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_15 --num_timesteps=10 --play' &
+#xterm -e 'python -m baselines.scn.run_humanoid --seed=11 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_11 --num_timesteps=10 --play' &
+#xterm -e 'python -m baselines.scn.run_humanoid --seed=12 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_12 --num_timesteps=10 --play' &
+#xterm -e 'python -m baselines.scn.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_13 --num_timesteps=10 --play' &
+#xterm -e 'python -m baselines.scn.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_14 --num_timesteps=10 --play' &
+#xterm -e 'python -m baselines.scn.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_15 --num_timesteps=10 --play' &
+#wait
+xterm -e 'python -m baselines.scn.run_humanoid --seed=1 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_1' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=2 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=3 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_3' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=4 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_4' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=5 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_5' &
+wait
+xterm -e 'python -m baselines.scn.run_humanoid --seed=6 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_6' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=7 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_7' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=8 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_8' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=9 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_9' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=10 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/scn_ho_10' &
 wait
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=16 --reward_scale=0.1 --env=Walker2d-v2' &
 #xterm -e 'python -m baselines.scn.run_humanoid --seed=17 --reward_scale=0.1 --env=Walker2d-v2' &
