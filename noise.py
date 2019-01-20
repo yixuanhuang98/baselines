0c6cf58351c2a85de3242085db83c068324937fa
gajeelsf <anepycn@gmail.com>
2019-01-19 22:04:43 -0800

    11-15 results

--- noise.py
+++ noise.py
@@ -7,9 +7,9 @@ for seed in seed_list:
 	f = open('noise.txt','a+')
 	f.write("\n----------%d----------\n" % seed)
 	f.close()
-	os.system("python -m baselines.scn.run_humanoid --seed="+str(seed)+" --reward_scale=1 --env="+env_id+ " --model-path=model/scn_ho_"+str(seed)+ " --num_timesteps=10 --play")
+	os.system("python -m baselines.fcn.run_humanoid --seed="+str(seed)+" --reward_scale=1 --env="+env_id+ " --model-path=model/fcn_ho_"+str(seed)+ " --num_timesteps=10 --play")
 
 	for std in std_list:
-		os.system("python -m baselines.scn.run_humanoid --seed="+str(seed)+" --reward_scale=1 --env="+env_id+ " --model-path=model/scn_ho_"+str(seed)+ " --num_timesteps=10 --play --acstd="+str(std))
+		os.system("python -m baselines.fcn.run_humanoid --seed="+str(seed)+" --reward_scale=1 --env="+env_id+ " --model-path=model/fcn_ho_"+str(seed)+ " --num_timesteps=10 --play --acstd="+str(std))
 	for std in std_list:
-		os.system("python -m baselines.scn.run_humanoid --seed="+str(seed)+" --reward_scale=1 --env="+env_id+ " --model-path=model/scn_ho_"+str(seed)+ " --num_timesteps=10 --play --obstd="+str(std))
+		os.system("python -m baselines.fcn.run_humanoid --seed="+str(seed)+" --reward_scale=1 --env="+env_id+ " --model-path=model/fcn_ho_"+str(seed)+ " --num_timesteps=10 --play --obstd="+str(std))
