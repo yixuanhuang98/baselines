fb8112d5c8a678b7b4d27b6682520e3b15a8254f
Gaurav Mahajan <gmahajan@eng.ucsd.edu>
2019-01-19 23:43:08 -0800

    Base

--- main.sh
+++ main.sh
@@ -1,5 +1,16 @@
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=1 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=0.1 --env=Walker2d-v2' &
-xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=0.1 --env=Walker2d-v2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=1 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_1' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=2 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_2' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=3 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_3' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=4 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_4' &
+xterm -e 'python -m baselines.fcn.run_humanoid --seed=5 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_5' &
+wait
+xterm -e 'python -m baselines.scn.run_humanoid --seed=1 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/scn_wk_1' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=2 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/scn_wk_2' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=3 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/scn_wk_3' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=4 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/scn_wk_4' &
+xterm -e 'python -m baselines.scn.run_humanoid --seed=5 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/scn_wk_5' &
+#xterm -e 'python -m baselines.fcn.run_humanoid --seed=6 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_6' &
+#xterm -e 'python -m baselines.fcn.run_humanoid --seed=7 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_7' &
+#xterm -e 'python -m baselines.fcn.run_humanoid --seed=8 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_8' &
+#xterm -e 'python -m baselines.fcn.run_humanoid --seed=9 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_9' &
+#xterm -e 'python -m baselines.fcn.run_humanoid --seed=10 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/fcn_wk_10' &
