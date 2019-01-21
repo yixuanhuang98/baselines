5ac5ab1a9bb0bfaa9a753952850bf41f02015a3b
gajeelsf <anepycn@gmail.com>
2019-01-20 17:18:51 -0800

    Fixed bug

--- main.sh
+++ main.sh
@@ -8,6 +8,11 @@ xterm -e 'python -m baselines.ppo1.run_humanoid --seed=12 --reward_scale=0.1 --e
 xterm -e 'python -m baselines.ppo1.run_humanoid --seed=13 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/mlp_ho_13 --num_timesteps=10 --play' &
 xterm -e 'python -m baselines.ppo1.run_humanoid --seed=14 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/mlp_ho_14 --num_timesteps=10 --play' &
 xterm -e 'python -m baselines.ppo1.run_humanoid --seed=15 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/mlp_ho_15 --num_timesteps=10 --play' &
+xterm -e 'python -m baselines.ppo1.run_humanoid --seed=1 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/mlp_wk_1 --num_timesteps=10 --play' &
+xterm -e 'python -m baselines.ppo1.run_humanoid --seed=2 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/mlp_wk_2 --num_timesteps=10 --play' &
+xterm -e 'python -m baselines.ppo1.run_humanoid --seed=3 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/mlp_wk_3 --num_timesteps=10 --play' &
+xterm -e 'python -m baselines.ppo1.run_humanoid --seed=4 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/mlp_wk_4 --num_timesteps=10 --play' &
+xterm -e 'python -m baselines.ppo1.run_humanoid --seed=5 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/mlp_wk_5 --num_timesteps=10 --play' &
 #xterm -e 'python -m baselines.ppo1relu64.run_humanoid --seed=1 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/mlp_ho_1' &
 #xterm -e 'python -m baselines.ppo1relu64.run_humanoid --seed=2 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/mlp_ho_2' &
 #xterm -e 'python -m baselines.ppo1relu64.run_humanoid --seed=3 --reward_scale=0.1 --env=Hopper-v2 --model-path=model/mlp_ho_3' &
