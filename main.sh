ef8b28833606ffb8fd937316a896cf96abffbb8e
gajeelSF <anepycn@gmail.com>
2019-01-21 15:11:02 -0800

    update script

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
-xterm -e 'python -m baselines.ppo1relu16.run_humanoid --seed=1 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/relu_wk_1' &
-xterm -e 'python -m baselines.ppo1relu16.run_humanoid --seed=2 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/relu_wk_2' &
-xterm -e 'python -m baselines.ppo1relu16.run_humanoid --seed=3 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/relu_wk_3' &
-xterm -e 'python -m baselines.ppo1relu16.run_humanoid --seed=4 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/relu_wk_4' &
-xterm -e 'python -m baselines.ppo1relu16.run_humanoid --seed=5 --reward_scale=0.1 --env=Walker2d-v2 --model-path=model/relu_wk_5' &
+#xterm -e 'python -m baselines.hyb.run_humanoid --seed=1 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/hyb_wk_1' &
+xterm -e 'python -m baselines.hyb.run_humanoid --seed=2 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/hyb_wk_2' &
+xterm -e 'python -m baselines.hyb.run_humanoid --seed=3 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/hyb_wk_3' &
+xterm -e 'python -m baselines.hyb.run_humanoid --seed=4 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/hyb_wk_4' &
+xterm -e 'python -m baselines.hyb.run_humanoid --seed=5 --reward_scale=1.0 --env=Walker2d-v2 --model-path=model/hyb_wk_5' &
