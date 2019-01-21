738de60a7f369c0f71604f4f950adacce6c944a6
gajeelSF <anepycn@gmail.com>
2019-01-21 13:14:25 -0800

    hybrid model

--- test3.py
+++
@@ -1,7 +0,0 @@
-import os
-env_id = "Swimmer-v2"
-seed_list = [1,2,3]
-reward_scale_list=[1]
-for seed in seed_list:
-    for reward_scale in reward_scale_list:
-        os.system('python -m baselines.fcn2.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
