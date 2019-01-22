2351a44787e83e6adbfe9dcd4bac27e6e94de2e0
gajeelSF <anepycn@gmail.com>
2019-01-21 22:04:11 -0800

    fix typo

--- main_stu.py
+++ main_stu.py
@@ -4,4 +4,4 @@ seed_list = [1,2,3,4,5]
 reward_scale_list=[0.1]
 for seed in seed_list:
     for reward_scale in reward_scale_list:
-        os.system('python -m baselines.stu.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id +'--model-path=model/hyb_hp_'+str(seed))
+        os.system('python -m baselines.stu.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id +' --model-path=model/hyb_hp_'+str(seed))
