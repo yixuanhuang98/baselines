0d3c4eca5fa6f26dd6091435a68a1cfef71f3c84
gajeelSF <anepycn@gmail.com>
2019-01-21 21:07:08 -0800

    update main file

--- main_hm.py
+++ main_hm.py
@@ -1,14 +1,7 @@
 import os
-env_id = "Humanoid-v2"
+env_id = "Hopper-v2"
 seed_list = [1,2,3,4,5]
-reward_scale_list=[0.1]
+reward_scale_list=[1.0]
 for seed in seed_list:
     for reward_scale in reward_scale_list:
-        #if seed is 4 or seed is 5:
-        #os.system('python -m baselines.fcn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
-        os.system('python -m baselines.scn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
-        #os.system('python -m baselines.ppo1.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
-        #os.system('python -m baselines.linear.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
-        #os.system('python -m baselines.nlfcn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
-        #os.system('python -m baselines.scn64.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
-	#os.system('python -m baselines.ppo1relu.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+        os.system('python -m baselines.hyb.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id +'--model-path=model/hyb_hp_'+str(seed))
