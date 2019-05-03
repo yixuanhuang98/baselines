bfdc3f09474db23a4fece33028f8548247daf83f
hyx <43940158+yixuanhuang98@users.noreply.github.com>
2019-05-03 08:40:48 -0700

    Add files via upload

---
+++ main_acrobot.py
@@ -0,0 +1,16 @@
+import os
+env_id = "Acrobot-v1"
+seed_list = [4]
+reward_scale_list=[0.1]
+for seed in seed_list:
+    for reward_scale in reward_scale_list:
+        #if seed is 3 or seed is 4 or seed is 5:
+        #os.system('python -m baselines.fcn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+        os.system('python -m baselines.fcn1.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id + ' --num_env=' + str(3))
+        #os.system('python -m baselines.ppo1.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+        #os.system('python -m baselines.linear.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+
+        #os.system('python -m baselines.nlfcn.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+        #os.system('python -m baselines.ppo1relu.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+        #os.system('python -m baselines.scn64.run_humanoid'+ ' --seed='+str(seed) + ' --reward_scale='+str(reward_scale) + ' --env=' + env_id)
+
